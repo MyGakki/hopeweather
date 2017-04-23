11553db2a0513b8598b22534cd5b5d263a465fef
MyGakki <1253126927@qq.com>
2017-04-23 17:02:34 +0800

    加入了显示天气的功能“ git commit -m 加入了显示天气的功能“

---
+++ app/src/main/java/com/hopepower/hopeweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.hopepower.hopeweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
